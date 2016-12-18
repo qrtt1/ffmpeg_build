18dfb32ff2a4b856429e3795521fb5d150bfd018
Ching Yi, Chan <chingyichan.tw@gmail.com>
2016-12-18 08:04:38 +0800

    build script

---
+++ build.sh
@@ -0,0 +1,12 @@
+origin=$(pwd)
+cd libmp3lame && ./configure --prefix=/opt/apps && make -j8 install
+cd $origin
+cd sdl && CC=build-scripts/gcc-fat.sh ./configure --prefix=/opt/apps && CC=build-scripts/gcc-fat.sh make -j8 install
+cd $origin
+cd x264 && ./configure --prefix=/opt/apps --enable-static && make -j8 install
+cd $origin
+cd ffmpeg && PATH=$PATH:/opt/apps/bin \
+    PKG_CONFIG_PATH=/opt/apps/lib/pkgconfig \
+    CFLAGS=-I/opt/apps/include \
+    LDFLAGS=-L/opt/apps/lib \
+    ./configure --prefix=/opt/ffmpeg --enable-libx264 --enable-libmp3lame --enable-gpl && make -j8 install
